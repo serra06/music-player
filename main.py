f6d7b1ff62562cb5e950ffadfc1fe1c0e880c95c
whydidbajidie <149108681+whydidbajidie@users.noreply.github.com>
2023-10-26 21:19:26 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+class MusicPlayer():
+    def __init__(self, song_name, singer):
+        self.song_name = song_name
+        self.singer = singer
+
+    # metot örneği
+    def sarki_ismi_degistir(self, value):
+        self.song_name = value
+
+song1 = MusicPlayer("abc", "singer_abc")
+print(song1.song_name, "şarkısının sanatçısı ", song1.singer)
+
+song1.sarki_ismi_degistir("def")
+print("değişim sonrası şarkı ismi")
+
+print(song1.song_name, "şarkısının sanatçısı ", song1.singer)
+
